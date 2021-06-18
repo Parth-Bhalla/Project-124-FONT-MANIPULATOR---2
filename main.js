5023ee925957b1008b0b391e172423d0264beba4
Parth-Bhalla <75519009+Parth-Bhalla@users.noreply.github.com>
2021-06-18 14:08:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+noseX=0;
+noseY=0;
+
+difference=0;
+rightWristX=0;
+leftWristX=0;
+
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,500);
+    canvas.position(550,150);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet model is intialize');
+}
